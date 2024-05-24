46e6898b5c5e892f403504397f7110737008cfd9
Zakhy <lirchzakhar@gmail.com>
2024-05-24 22:53:52 +0700

    add python dotenv

--- education_platform/conf/prod.py
+++ education_platform/conf/prod.py
@@ -2,6 +2,8 @@ import os
 
 from pathlib import Path
 
+from dotenv import load_dotenv
+
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 ADMINS = [
     ('Zakhy M', 'lirchzakhar@gmail.com')
@@ -9,23 +11,21 @@ ADMINS = [
 
 ALLOWED_HOSTS = ['*']
 
+dotenv_path = BASE_DIR / '.env'
+load_dotenv(dotenv_path)
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ.get('POSTGRES_DB'),
         'USER': os.environ.get('POSTGRES_USER'),
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-        'HOST': 'db',
+        'HOST': 'localhost',
         'PORT': 5432,
     }
 }
 
-REDIS_URL = 'redis://redis:6379'
-
-CACHES['default']['LOCATION'] = REDIS_URL
-CHANNEL_LAYERS['default']['CONFIG']['host'] = [REDIS_URL]
-
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
-SECURE_SSL_REDIRECT = False
+SECURE_SSL_REDIRECT = True
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
